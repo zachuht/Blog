de9465f8dc25b8a528d9528d63c7deb91de78e91
Zachary <zachuht@gmail.com>
2019-05-05 18:20:13 -0700

    fix register route error

--- models/blog.js
+++ models/blog.js
@@ -1,11 +1,13 @@
 var mongoose = require("mongoose");
 
 var blogSchema = new mongoose.Schema({
+  username: String,
   title: String,
   image: String,
   body: String,
   created: { type: Date, default: Date.now },
 });
 var Blog = mongoose.model("Blog", blogSchema);
+console.log(Blog);
 
 module.exports = mongoose.model("Blog", blogSchema);
