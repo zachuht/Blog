33dad8431800206eb616a79c8a0066bea4eb2ac0
Zachary <zachuht@gmail.com>
2019-05-05 18:24:19 -0700

    remove console.log

--- models/blog.js
+++ models/blog.js
@@ -8,6 +8,5 @@ var blogSchema = new mongoose.Schema({
   created: { type: Date, default: Date.now },
 });
 var Blog = mongoose.model("Blog", blogSchema);
-console.log(Blog);
 
 module.exports = mongoose.model("Blog", blogSchema);
